6a84b91e06f709240300ee300509671cf3314b08
Tesfahunenge <tsweetam@gmail.com>
2022-09-09 14:47:34 -0700

    my correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+*  main - print the string in put function
+*  
+*  Description: using the main function
+*  this program print "Programming is like building a multilingual puzzle
+*  Return: 0
+*/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }
