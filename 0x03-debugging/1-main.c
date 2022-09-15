a05d050cdacaf847da9c3e2e4363f44dd8b9f0d0
Tesfahunenge <tsweetam@gmail.com>
2022-09-15 07:27:17 -0700

    my commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -cause an infinite loop
+ *
+ * Retirn: 0
+ */
+int main(void)
+{
+       int i;
+
+       printif("Infinite loop incoming :(\n");
+
+       i = 0;
+       
+       printf("Infinite loop avoided! \\o/\n");
+       return (0); 
+}
