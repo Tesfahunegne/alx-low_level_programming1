98c2059fb5ffa5e4377571602caca1a9a8319b22
Tesfahunenge <tsweetam@gmail.com>
2022-09-16 06:55:40 -0700

    my comminet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,19 @@
-#ifndef TESFA
-#define TESFA
-int _putchar(char);
+#include "main.h"
+/**
+* main - print putchar
+* Return: alwayse 0
+*/
+int main(void)
+{
+      _putchar('_');
+      _putchar('p');
+      _putchar('u');
+       _putchar('t');
+        _putchar('c');
+         _putchar('h');
+       _putchar('a');
+    _putchar('r');
+ _putchar('\n');
 
-#endif
+return (0);
+}
