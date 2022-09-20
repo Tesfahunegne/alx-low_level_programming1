3d43ce011cd03bf03c30e6425de1b1953551e0bf
Tesfahunenge <tsweetam@gmail.com>
2022-09-20 06:49:31 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - man function fabunace
+ * Description: print fabunance sequence
+ * Return: o
+ */
+int main(void)
+{
+	int i;
+	int n = 50;
+	int t1 = 1, t2 = 2;
+	int nextTerm = t1 + t2;
+
+	printf("Fibonacci Series:%d,%d,", t1, t2);
+	for (i = 3; i <= n; ++i)
+	{
+	       printf("%d,", nextTerm);
+	       
+	       t1 = t2;
+	       t2 = nextTerm;
+	       nextTerm = t1 + t2;
+	}
+	return 0;
+}
