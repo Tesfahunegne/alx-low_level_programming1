8ecb0aea98f505df5b3f88964a6ab18f0c63be34
Tesfahunenge <tsweetam@gmail.com>
2022-09-20 13:59:14 -0700

    final

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i;
 	int n = 50;
-	int t1 = 1, t2 = 2;
-	int nextTerm = t1 + t2;
+	long int t1 = 1, t2 = 2;
+	long int nextTerm = t1 + t2;
 	
-	printf("%d, %d, ", t1, t2);
+	printf("%lu, %lu, ", t1, t2);
 	for (i = 3; i <= n; ++i)
 	{
-	       printf("%d, ", nextTerm);
+	       printf("%lu, ", nextTerm);
 	       
 	       t1 = t2;
 	       t2 = nextTerm;
