effa6f8691b729be32cda6d76ec4f873ab9508a0
Tesfahunenge <tsweetam@gmail.com>
2022-09-20 07:00:57 -0700

    my commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,11 +10,11 @@ int main(void)
 	int n = 50;
 	int t1 = 1, t2 = 2;
 	int nextTerm = t1 + t2;
-
-	printf("%d,%d,", t1, t2);
+	
+	printf("%d, %d, ", t1, t2);
 	for (i = 3; i <= n; ++i)
 	{
-	       printf("%d,", nextTerm);
+	       printf("%d, ", nextTerm);
 	       
 	       t1 = t2;
 	       t2 = nextTerm;
