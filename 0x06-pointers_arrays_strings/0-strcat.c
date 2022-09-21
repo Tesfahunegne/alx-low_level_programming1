bcab3abc9e4764f0daf01274fc6ab39bcf052baa
Tesfahunenge <tsweetam@gmail.com>
2022-09-21 10:24:08 -0700

    my commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,17 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int a = -1, i;
 
-	while (dest[index++])
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		dest_len++;
 	}
-	for (index = 0; src[index]; index++)
-	{
-		dest[dest_len++] = src[index];
-	}
+
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+	_putchar('\n');
 	return (dest);
 }
