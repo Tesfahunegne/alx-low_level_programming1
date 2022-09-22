dafd9a56cebb65bd0699fa504b0f15853b02bc2e
Tesfahunenge <tsweetam@gmail.com>
2022-09-21 19:07:32 -0700

    my commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,31 @@
  * Return: A pointer to the destination string @dest
  */
 char *_strncat(char *dest, char *src, int n)
-{
-	int index = 0, dest_len = 0;
 
-	while (dest[index++])
-	{
-		dest_len++;
-	}
-	for (index = 0; src[index]; index++)
-	{
-		dest[dest_len++] = src[index];
-	}
+{
+
+
+
+	int dest_len, i;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+
+		;
+
+
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+
+			dest[dest_len + i] = src[i];
+
+
+
+	/*should end with a end of string char*/
+
+	dest[dest_len + i] = '\0';
+
+
+
 	return (dest);
+
 }
