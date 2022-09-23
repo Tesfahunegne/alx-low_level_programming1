434ea456171971ccdfccc066fd2bbc8649b880fa
Tesfahunenge <tsweetam@gmail.com>
2022-09-23 04:39:39 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - Entry point
+ * @r: pointer
+ * Return: Always 0 (Success)
+ */
+char *rot13(char *r)
+{
+	int i;
+	int j;
+	char k[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char n[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0 ; *(r + i) != '\0' ; i++)
+	{
+		for (j = 0 ; k[j] != '\0'; j++)
+		{
+			if (*(r + i) == k[j])
+			{
+				if (*(r + i) == k[j])
+				{
+					*(r + i) = n[j];
+					break;
+				}
+			}
+		}
+	}
+	return (r);
+}
