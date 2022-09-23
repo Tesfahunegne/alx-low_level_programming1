176e7486eceb9ed9156387da082bdcfe2b44c389
Tesfahunenge <tsweetam@gmail.com>
2022-09-23 06:02:40 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_number - print a number
+ * @n: accepet intiger
+ * Return : return 0
+ */
+void print_number(int n)
+{
+	int i = n;
+
+	_putchar(i);
+	return (i);
+}
