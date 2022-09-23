181571a0991687b1eacb1dcc19dce79f219b8451
Tesfahunenge <tsweetam@gmail.com>
2022-09-23 05:14:06 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+int main(void)
+{
+
+	    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+	       
+
+		   rot13(s);
+
+		    return (0);
+}
