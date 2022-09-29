2b43e024643d1705ea0ed9bd61d18e4faf96efca
Tesfahunenge <tsweetam@gmail.com>
2022-09-29 01:00:35 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,49 +5,25 @@
  * @n: int
  * Return: 0 or 1
  */
-
-int check_prime(int i,int num)                                                                                                                                                                  
-
-{                                                                                                                                                                                               
-
-	        if(num==i)                                                                                                                                                                              
-
-			                                                                                                                                                                                                
-
-			                        return 0;                                                                                                                                                               
-
-		                                                                                                                                                                                                
-
-		            else                                                                                                                                                                                
-
-				                                                                                                                                                                                                    
-
-				                                if(num%i==0)                                                                                                                                                        
-
-									                                                                                                                                                                                                
-
-									                                                return 1;                                                                                                                                       
-
-			                                                                                                                                                                                                    
-
-			                    else{                                                                                                                                                                           
-
-						                                                                                                                                                                                                    
-
-						                                    return  check_prime(i+1,num);                                                                                                                                   
-
-										                                                                                                                                                                                                    
-
-										                                        }                                                                                                                                                           
-
+int check_prime(int i, int num)
+{
+	if (num == i)
+		return (0);
+	else
+		if (num % i == 0)
+			return (1);
+		else
+		{
+			return  (check_prime(i + 1, num));
+		}
 }
 int is_prime_number(int n)
 
 {
-	if(check_prime(2,n)==0)
-		return(1);
+	if (check_prime(2, n) == 0)
+		return (1);
 	else
-		return(0);
+		return (0);
 }
 /**
  * check_prime - check all number < n if they can divide it
