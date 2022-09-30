9aa7555e65a6bd8bdbbcd1e73e839d08296b6509
Tesfahunenge <tsweetam@gmail.com>
2022-09-30 10:21:39 -0700

    ar -t libmy.a

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcat - Concatenates the string pointed to by @src,
+ * including the terminating
+ * @dest: A pointer to the string to be concatenated upon
+ * @src: The source string to be appended to @dest
+ *
+ * Return: A pointer to the destination string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = -1, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+	return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: int ab from the function
+ * Return: absolute value of abReturn: absolute value of ab
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
