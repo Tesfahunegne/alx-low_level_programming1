5d490dd4fb2f870fa00400a27bb136ee7f5373e7
Tesfahunenge <tsweetam@gmail.com>
2022-09-30 11:05:29 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function that print the name of the function
+ * @argc: argument
+ * @argv: argument victor
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
