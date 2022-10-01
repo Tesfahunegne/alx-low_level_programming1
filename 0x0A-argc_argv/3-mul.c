40935bafa7fdcca235351bd40506100154effdeb
Tesfahunenge <tsweetam@gmail.com>
2022-09-30 23:56:25 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main functionn that print the number of
+ * argument passed to the function
+ * @args: argument
+ * @argv: argument victor
+ * Return: 0
+ */
+int main(int args, char *argv[])
+{
+	int n1 n2 n3;
+
+	if (args != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n1 = antio(argv[1]);
+	n2 = antio(argv[2]);
+	n3 = n1 * n2;
+	printf("%d\n", n3);
+	return (0);
+}
