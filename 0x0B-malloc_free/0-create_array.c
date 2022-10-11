1e42574c83f92dec88d5abfde1a83658eb48aa1a
Tesfahunenge <tsweetam@gmail.com>
2022-10-11 05:39:54 -0700

    my commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,11 +8,24 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *m = malloc(size);
+	char *s;
+	unsigned int i;
 
-	if (size == 0 || m == 0)
-		return (0);
-	while (size--)
-		m[size] = c;
-	return (m);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc((size) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
 }
