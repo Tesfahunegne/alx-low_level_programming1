c0497e31e9d52027b876a319ddaf9e04c954064b
Tesfahunenge <tsweetam@gmail.com>
2022-12-18 22:57:31 -0800

    my commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *
+ *  * set_bit - set_bit
+ *
+ *   * @n: pointers
+ *
+ *    * @index: variable
+ *
+ *     * Return: Value
+ *
+ *      * Description: Fuction
+ *
+ *       */
+
+int set_bit(unsigned long int *n, unsigned int index)
+
+{
+
+
+
+		if (index > sizeof(int) * 8)
+
+				{
+
+							return (-1);
+
+								}
+
+
+
+			*n = (*n | 1 << index);
+
+
+
+				return (1);
+
+}
